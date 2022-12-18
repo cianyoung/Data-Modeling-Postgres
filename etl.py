92ee4d38abb9aef8183a720f3231606b26b99fda
Cian Young <cian.young@workday.com>
2022-12-18 21:57:18 +0000

    ETL working

--- etl.py
+++ etl.py
@@ -3,41 +3,53 @@ import glob
 import psycopg2
 import pandas as pd
 from sql_queries import *
-
+from datetime import datetime
 
 def process_song_file(cur, filepath):
+    """function processes song file and inserts data into song table and artists table."""
     # open song file
-    df = 
+    df = pd.read_json(filepath, lines= True)
 
-    # insert song record
-    song_data = 
-    cur.execute(song_table_insert, song_data)
-    
     # insert artist record
-    artist_data = 
+    artist_data = df[["artist_id", "artist_name", "artist_location", "artist_latitude", "artist_longitude"]].values[0].tolist()
     cur.execute(artist_table_insert, artist_data)
 
+    # insert song record
+    song_data = df[["song_id", "title", "artist_id", "year", "duration"]].values[0].tolist()
+    cur.execute(song_table_insert, song_data)
 
 def process_log_file(cur, filepath):
-    # open log file
-    df = 
+    """functions processes log file and inserts data into tables time,user and songplay."""
+    # open log file .
+    df = pd.read_json(filepath, lines= True)
 
     # filter by NextSong action
-    df = 
+    df = df.loc[df["page"] == "NextSong"]
 
     # convert timestamp column to datetime
-    t = 
+    t = pd.to_datetime(df['ts'], unit='ms')
+
+
+    timestamp = t.values.tolist()
+    hour = t.dt.hour.values.tolist()
+    day = t.dt.day.values.tolist()
+    week_of_year = t.dt.isocalendar().week
+    month = t.dt.month.values.tolist()
+    year = t.dt.year.values.tolist()
+    weekday = t.dt.dayofweek.values.tolist()
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = list(zip(timestamp, hour, day, week_of_year, month, year, weekday))
+    column_labels = ['timestamp', 'hour', 'day', 'week_of_year', 'month', 'year', 'weekday']
+    time_df = pd.DataFrame(time_data, columns=column_labels)
+    time_df['timestamp'] = time_df['timestamp'].apply(pd.to_datetime)
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
+    user_df = user_df.drop_duplicates(subset=None)
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -45,22 +57,21 @@ def process_log_file(cur, filepath):
 
     # insert songplay records
     for index, row in df.iterrows():
-        
+
         # get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
-        
         if results:
             songid, artistid = results
         else:
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
-        cur.execute(songplay_table_insert, songplay_data)
-
+        songplay_data = (pd.to_datetime(row.ts, unit='ms'), int(row.userId), row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
+    cur.execute(songplay_table_insert, songplay_data)
 
 def process_data(cur, conn, filepath, func):
+    """function passes all the files under a filepath to given function as argument and executes the given function."""
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
